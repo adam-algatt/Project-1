5e33e314ced9ba89a292c4e976ffb2d718ebaef6
adam-algatt <adam.algatt@gmail.com>
2022-03-15 15:45:41 -0700

    added logic and working weather forecast formatting

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,7 +1,7 @@
 // //var URL = "https://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&appid=a22e1d322f701f954bc18b4b1b6801cc"
 
 
-var forecastObj = {};
+var forecastArr = [];
 
 // //array containing latitude and longitude for cities covered by app
 // //lat and long vals are currently filler info **MAKE SURE TO CHANGE** before MVP
@@ -58,43 +58,63 @@ var forecastObj = {};
 // ];
 
 //once city selection is added to the dropdown menu the url below will be added in between the fetch function '()' 
-var url = "https://weatherbit-v1-mashape.p.rapidapi.com/forecast/3hourly?lat=" + latitude + "&lon=" + Longitude + "&units=I&days=5";
+//var url = "https://weatherbit-v1-mashape.p.rapidapi.com/forecast/3hourly?lat=" + latitude + "&lon=" + Longitude + "&units=I&days=5";
+
+//variables used in injectHtml()
+var weatherEl = document.getElementsByClassName('weather');
 
 
 //function takes key val pairs from forcastObj and injects info into HTML #weather . 
-function injectHtml(obj) {
-let temp 
+function injectHtml(arr) {
+  for (i=0; i<arr.length; i++){
+let temp = arr[i].temp;
+let relativeHumidity = arr[i].relativeHumidity + '%';
+let description = arr[i].desc;
+weatherEl.setAttribute('class', 'weather  center-align col s12');
+let weatherDiv = document.createElement('div');
+weatherEl.appendChild(weatherDiv);
+let weatherUl = document.createElement('ul');
+document.createElement('li');
+li.textContent = description;
+document.createElement('li');
+li.textContent = temp;
+document.createElement('li');
+li.textContent = relativeHumidity;
+weatherUl.appendChild('li'); 
+weatherDiv.appendChild(weatherUl);
 
-
+  }
 }; 
 
 
 function forecast(data) {
   console.log(data);
-  console.log('temperature:', data.data[0].temp);
-  console.log('description:', data.data[0].weather.description);
   // console.log(data.data[0].temp);
   // console.log(data.data[0].temp);
-
-  let temp = data.data[0].temp;
-
-  let relativeHumidity = data.data[0].temp + '%';
-  let description = data.data[0].weather.description;
+for (i=0; i < data.data.length; i++){
+let forecastObj = {};
+  let temp = data.data[i].temp;
+  let relativeHumidity = data.data[i].temp + '%';
+  let description = data.data[i].weather.description;
   Object.assign(forecastObj, {
-    'description': description,
-    'temperature': temp,
+    'desc': description,
+    'temp': temp,
    'relative-humidity': relativeHumidity
   });
-   console.log(forecastObj);
-   injectHtml(forecastObj);
-  return forecastObj;
+  forecastArr.push(forecastObj);
+  forecastObj = {}; 
+}
+   //console.log(forecastObj);
+   //injectHtml(forecastArr);
+  console.log(forecastArr);
+  return forecastArr;
 };
 
 // //API fetch gets weather data and calls forecast function to format data 
 // //Eventually this will be a concatinated string that selects a latitude/longitude based on city selection by user
 
 
-fetch("https://weatherbit-v1-mashape.p.rapidapi.com/forecast/3hourly?lat=35.5&lon=-78.5&units=I&days=5", {
+fetch("https://weatherbit-v1-mashape.p.rapidapi.com/forecast/daily?lat=35.5&lon=-78.5&units=I&days=10", {
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-host": "weatherbit-v1-mashape.p.rapidapi.com",
