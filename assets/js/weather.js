a26a999bc722e6e1a40e55af488b89141388a47e
adam-algatt <adam.algatt@gmail.com>
2022-03-17 14:57:54 -0700

    weather section cards dynamically populate from weather api

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -41,131 +41,66 @@ var coordinates = {
   "Tualatin": "?lat=45.2&lon=-122.5"
 };
 
-// //array containing latitude and longitude for cities covered by app
-// //lat and long vals are currently filler info **MAKE SURE TO CHANGE** before MVP
-// var coordinates = [{
-//     name: 'Portland',
-//     latitude: 15,
-//     longitude: 15
-//   },
-//   {
-//     name: 'Eugene',
-//     latitude: 44.37,
-//     longitude: -123.58
-//   },
-//   {
-//     name: 'Bend', //     ,K Falls 
-//     latitude: 44.05,
-//     longitude: -121.31
-//   },
-//   {
-//     name: Newport,
-//     latitude: 44.63,
-//     longitude: -124.05
-//   },
-//   {
-//     name: Astoria,
-//     latitude: 46.18,
-//     longitude: -123.83
-//   },
-//   {
-//     name: Salem,
-//     latitude: 44.94,
-//     longitude: -123.04
-//   },
-//   {
-//     name: Medford,
-//     latitude: 42.32,
-//     longitude: -122.87
-//   },
-//   {
-//     name:  LaGrande,
-//     latitude: 45.32,
-//     longitude: -118.08
-//   },
-//   {
-//   name:  Pendelton,
-//   latitude: 45.67,
-//   longitude: -118.78
-// },
-// {
-// name:  Klamath-Falls,
-// latitude: 42.22,
-// longitude: -121.78
-// }
-// ];
 
 //once city selection is added to the dropdown menu the url below will be added in between the fetch function '()' 
 //var url = "https://weatherbit-v1-mashape.p.rapidapi.com/forecast/3hourly?lat=" + latitude + "&lon=" + Longitude + "&units=I&days=5";
 
 
 //variables used in injectHtml()
-var weatherEl = document.getElementsByClassName('weather');
+
 
 
 //function takes key val pairs from forcastObj and injects info into HTML #weather . 
 function injectHtml(arr) {
-  for (i=0; i<arr.length; i++){
-let temp = arr[i].temp;
-let relativeHumidity = arr[i].relativeHumidity + '%';
-let description = arr[i].desc;
-weatherEl.setAttribute('class', 'weather  center-align col s12');
-let weatherDiv = document.createElement('div');
-weatherEl.appendChild(weatherDiv);
-let weatherUl = document.createElement('ul');
-document.createElement('li');
-li.textContent = description;
-document.createElement('li');
-li.textContent = temp;
-document.createElement('li');
-li.textContent = relativeHumidity;
-weatherUl.appendChild('li'); 
-weatherDiv.appendChild(weatherUl);
+  for (i = 0; i < 5; i++) {
+    let idQuery = "forecast-" + i;
+    console.log(idQuery);
+    let forecastDay = document.getElementById(idQuery);
+    console.log(forecastDay);
+    let temp = arr[i].temp;
+    let relativeHumidity = arr[i].humidity;
+    let description = arr[i].desc;
+    let wind = arr[i].wind;
+  let degreeSymbol = String.fromCharCode(176) + 'F';
+    let forecastDescription = document.getElementById("forecast-title-" + i);
+    let forecastTemp = document.getElementById("temp-" + i);
+    let forecastRelativeHumidity = document.getElementById("humidity-" + i);
+    let forecastWind = document.getElementById("wind-" + i);
+  console.log(forecastDescription, forecastTemp, forecastRelativeHumidity);
+    
+  forecastDescription.innerText = description;
+  forecastTemp.innerText = temp + degreeSymbol;
+  forecastRelativeHumidity.innerText = relativeHumidity;
+  forecastWind.innerText = wind;
 
   }
-}; 
-
+};
 
 function forecast(data) {
   console.log(data);
 
   // console.log(data.data[0].temp);
   // console.log(data.data[0].temp);
-for (i=0; i < data.data.length; i++){
-let forecastObj = {};
-  let temp = data.data[i].temp;
-  let relativeHumidity = data.data[i].temp + '%';
-  let description = data.data[i].weather.description;
-  Object.assign(forecastObj, {
-    'desc': description,
-    'temp': temp,
-   'relative-humidity': relativeHumidity
-  });
-  forecastArr.push(forecastObj);
-  forecastObj = {}; 
-}
-   //console.log(forecastObj);
-   //injectHtml(forecastArr);
+  for (i = 0; i < data.data.length; i++) {
+    let forecastObj = {};
+    let temp = data.data[i].temp;
+    let humidity = data.data[i].rh + '%';
+    let description = data.data[i].weather.description;
+    let wind = data.data[i].wind_cdir + ' ' + data.data[i].wind_spd + "mph";
+    Object.assign(forecastObj, {
+      'desc': description,
+      'temp': temp,
+      'humidity': humidity,
+      'wind': wind
+    });
+    forecastArr.push(forecastObj);
+    forecastObj = {};
+  }
+
   console.log(forecastArr);
+
+  injectHtml(forecastArr);
   return forecastArr;
-
-  console.log('temperature:', data.data[0].temp);
-  console.log('description:', data.data[0].weather.description);
-  // console.log(data.data[0].temp);
-  // console.log(data.data[0].temp);
-
-  let temp = data.data[0].temp;
-
-  let relativeHumidity = data.data[0].temp + '%';
-  let description = data.data[0].weather.description;
-  Object.assign(forecastObj, {
-    'description': description,
-    'temperature': temp,
-   'relative-humidity': relativeHumidity
-  });
-   console.log(forecastObj);
-   //injectHtml(forecastObj);
-  return forecastObj;
 
 };
 
